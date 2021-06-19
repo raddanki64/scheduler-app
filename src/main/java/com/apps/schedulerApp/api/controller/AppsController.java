8f157740c764d006455fb8660bd81eca6a979445
raddanki64 <addanki@yahoo.com>
2021-06-18 20:53:52 -0400

    Restful api in heroku

---
+++ src/main/java/com/apps/schedulerApp/api/controller/AppsController.java
@@ -0,0 +1,36 @@
+package com.apps.schedulerApp.api.controller;
+
+
+import  io.swagger.annotations.Api;
+import  io.swagger.annotations.ApiOperation;
+import  io.swagger.annotations.ApiResponse;
+import  io.swagger.annotations.ApiResponses;
+
+import  lombok.AllArgsConstructor;
+import  org.slf4j.Logger;
+import  org.slf4j.LoggerFactory;
+import  org.springframework.beans.factory.annotation.Autowired;
+import	org.springframework.http.HttpHeaders;
+import  org.springframework.http.HttpStatus;
+import  org.springframework.http.MediaType;
+import  org.springframework.http.ResponseEntity;
+import  org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value="", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(value = "Return poc settings", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
+
+public class AppsController {
+    private static Logger logger = LoggerFactory.getLogger(AppsController.class);
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Say hello!")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = String.class)})
+    public ResponseEntity<?> sayHelo() throws Exception {    	
+        String resource = "Hello! Family, Happy Fathers Day!!";
+        return new ResponseEntity<>(resource, HttpStatus.OK);
+    }
+}
